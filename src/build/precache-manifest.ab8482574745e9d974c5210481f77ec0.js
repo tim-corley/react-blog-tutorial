34f9d4ea51a3964f022403e5839290e369832a00
Tim Corley <tcorley215@gmail.com>
2020-02-25 14:30:29 -0500

    updaing server.js, adding build folder

---
+++ src/build/precache-manifest.ab8482574745e9d974c5210481f77ec0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c505c7ef5423be69a39903d4e629dcc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e1c7ab1d0bbb8568068",
+    "url": "/static/css/main.bbd23df2.chunk.css"
+  },
+  {
+    "revision": "9904391eefd72d4b58de",
+    "url": "/static/js/2.07087ddd.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.07087ddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e1c7ab1d0bbb8568068",
+    "url": "/static/js/main.8bbf883a.chunk.js"
+  },
+  {
+    "revision": "ca5230706a3fd03f7012",
+    "url": "/static/js/runtime-main.4180fa2e.js"
+  }
+]);
